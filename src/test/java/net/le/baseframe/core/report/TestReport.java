34c094c633f19a34a77d7fbadb5b96c9376966a3
hanl1946@163.com <hanl1946@163.com>
2018-11-23 00:10:15 +0800

    构建提交

--- src/test/java/net/le/baseframe/core/report/TestReport.java
+++ src/test/java/net/le/baseframe/core/report/TestReport.java
@@ -1,6 +1,5 @@
 package net.le.baseframe.core.report;
 
-import com.sun.javafx.collections.MappingChange;
 import lombok.extern.slf4j.Slf4j;
 import net.le.baseframe.core.dao.ReportDao;
 import net.le.baseframe.core.service.ReportService;
@@ -12,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.List;
 import java.util.Map;
 
 @RunWith(SpringRunner.class)
